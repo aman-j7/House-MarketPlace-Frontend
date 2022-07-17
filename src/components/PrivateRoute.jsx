a24406383bc8c225144fffae11cbe863304cc295
aman-j7 <463amanj@gmail.com>
2022-07-18 00:46:34 +0530

    build version

--- src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRoute = () => {
-  const loggedIn = useSelector((state) => state.userReducer.loggedIn);
-  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
-};
-
-export default PrivateRoute;
